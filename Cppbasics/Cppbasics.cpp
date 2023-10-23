a842d3d320aed8912ddf5eced4d5dfc0f02e890f
siyeon <aeiou0302@icloud.com>
2023-10-23 21:07:00 +0900

    초기 커밋.

---
+++ Cppbasics/Cppbasics.cpp
@@ -0,0 +1,15 @@
+﻿// Cppbasics.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//                  깃허브 연동을 테스트하기위한 프로젝트입니다.
+
+#include <stdio.h>
+#include <iostream>
+
+
+int main()
+{
+    printf("Hello, World\n"); //C
+    std::cout << "Hello World!\n"; //C++
+
+    return 0;
+}
+
